5b363500496fe22d94fc51319ca9ec9eaa5440f7
Benjamin Füldner <benjamin@fueldner.net>
2019-09-05 14:27:52 +0200

    Add regular expression to node.Element() and node.Attribute() to remove line feeds inside text.

--- pysvd/node.py
+++ pysvd/node.py
@@ -1,13 +1,16 @@
+import re
+
+
 def Element(node, tag, mandatory=False):
     """Get the element text for the provided tag from the provided node"""
 
     value = node.findtext(tag)
     if value is None:
         if mandatory:
-            raise SyntaxError("Element '{}.{}' is mandatory, \
-                               but not present!".format(node.tag, tag))
+            raise SyntaxError("Element '{}.{}' is mandatory, but not present!".format(node.tag, tag))
         return None
     else:
+        value = re.sub(r'\s+', ' ', value)
         return value.strip()
 
 
@@ -17,8 +20,8 @@ def Attribute(node, tag, mandatory=False):
     value = node.get(tag)
     if value is None:
         if mandatory:
-            raise SyntaxError("Attribute '{}@{}' is mandatory, \
-                               but not present!".format(node.tag, tag))
+            raise SyntaxError("Attribute '{}@{}' is mandatory, but not present!".format(node.tag, tag))
         return None
     else:
+        value = re.sub(r'\s+', ' ', value)
         return value.strip()
